8902f3ad0ea0eab491cb68637762cbc236c7a3eb
cmhbenjamin <minghong@uw.edu>
2013-11-30 01:13:51 -0800

    added example code to sender

--- Project3/src/chatroom/serverless/MulticastClient.java
+++ Project3/src/chatroom/serverless/MulticastClient.java
@@ -15,41 +15,30 @@ public class MulticastClient implements Runnable{
 	// TODO: send out message that this user is on the network
 	// TODO: handle received messages and print them\
 	// Which port should we listen to
-	public static int port = 5000;
-	// Which address
-	public static String group = "225.4.5.6";
+	private MulticastSocket mcs;
 	
-	public MulticastClient(){
-		
+	public MulticastClient(MulticastSocket mcs){
+		this.mcs=mcs;
 	}
-	
-	public void run(){
-		try{
-			// Create the socket and bind it to port 'port'.
-			MulticastSocket s = new MulticastSocket(port);
-			// join the multicast group
-			s.joinGroup(InetAddress.getByName(group));
-			// Now the socket is set up and we are ready to receive packets
-			// Create a DatagramPacket and do a receive
+
+	@Override
+	public void run() {
+		
+		try {
 			byte buf[] = new byte[1024];
 			DatagramPacket pack = new DatagramPacket(buf, buf.length);
-			s.receive(pack);
-			// Finally, let us do something useful with the data we just received,
-			// like print it on stdout :-)
-			System.out.println("Received data from: " + pack.getAddress().toString() +
-					    ":" + pack.getPort() + " with length: " +
-					    pack.getLength());
-			System.out.write(pack.getData(),0,pack.getLength());
-			System.out.println();
-			// And when we have finished receiving data leave the multicast group and
-			// close the socket
-			s.leaveGroup(InetAddress.getByName(group));
-			s.close();
-		}catch(IOException e){
+			while(true){
+				mcs.receive(pack);
+				System.out.println("Received data from: " + pack.getAddress().toString() +
+						    ":" + pack.getPort() + " with length: " +
+						    pack.getLength());
+				System.out.write(pack.getData(),0,pack.getLength());
+				System.out.println();
+			}
+			
+		} catch (IOException e) {
+			
 			e.printStackTrace();
-		}
+		}	
 	}
-	
-	
-	
 }
